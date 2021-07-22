ca5f59b76850dcd2bdf9aa425b604ab4310c01a3
Giri Priyadarshan <43517605+giripriyadarshan@users.noreply.github.com>
2021-07-22 13:03:23 +0530

    Added adobe pdf image for the file list

--- notes-app-csharp-wpf/Pages/Contents.xaml.cs
+++ notes-app-csharp-wpf/Pages/Contents.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Data;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 using static notes_app_csharp_wpf.commons;
 
 namespace notes_app_csharp_wpf.Pages
@@ -19,27 +22,26 @@ namespace notes_app_csharp_wpf.Pages
         {
             connection.Open();
 
-            MenuItem sem = new MenuItem() { Title = "Semester" };
+            var sem = new MenuItem() {Title = "Semester"};
 
-            DataTable dt = new DataTable();
+            var dt = new DataTable();
             Set_Command("SELECT * FROM semester");
             _ = da.Fill(dt);
 
             foreach (DataRow dtrow in dt.Rows)
             {
-
-                MenuItem sub = new MenuItem()
+                var sub = new MenuItem()
                 {
                     Title = dtrow["semester"].ToString(),
                 };
 
                 Set_Command("SELECT * FROM subject WHERE semesterID=" + dtrow["Id"]);
-                DataTable s = new DataTable();
+                var s = new DataTable();
                 _ = da.Fill(s);
 
                 foreach (DataRow dtrow2 in s.Rows)
                 {
-                    MenuItem year = new MenuItem()
+                    var year = new MenuItem()
                     {
                         Title = dtrow2["subject_name"].ToString(),
                     };
@@ -48,25 +50,23 @@ namespace notes_app_csharp_wpf.Pages
                         "SELECT * FROM year WHERE semesterID=" +
                         dtrow2["semesterID"] +
                         " AND subjectID=" + dtrow2["Id"]
-                        );
-                    DataTable y = new DataTable();
+                    );
+                    var y = new DataTable();
                     _ = da.Fill(y);
 
                     foreach (DataRow dtrow3 in y.Rows)
                     {
                         year.Items.Add(new MenuItem()
                         {
-                            Title = dtrow3["year"].ToString(),
+                            Title = dtrow3["year"].ToString() + dtrow3["filename"],
                             Index = dtrow3["Id"].ToString()
                         });
                     }
 
                     sub.Items.Add(year);
-
                 }
 
                 sem.Items.Add(sub);
-
             }
 
             _ = MainList.Items.Add(sem);
@@ -76,37 +76,44 @@ namespace notes_app_csharp_wpf.Pages
 
         private void MainList_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
         {
-            MenuItem selectedItem = e.NewValue as MenuItem;
-            if (selectedItem != null)
+            if (!(e.NewValue is MenuItem selectedItem)) return;
+            // create a template to display file-names using listbox
+            // double click on the item of the list will open th e pdf file associated with the name
+            // to open a file use
+            // System.Diagnostics.Process.Start(@"|DataDirectory|\\Resources\\files\\" + selectedItem.Index);
+            FileList.Items.Clear();
+            var files = new DataTable();
+            Set_Command("SELECT * FROM year WHERE Id='" + selectedItem.Index + "'");
+            connection.Open();
+            _ = da.Fill(files);
+            var x = new Image
             {
-                // create a template to display file-names using listbox
-                // double click on the item of the list will open th e pdf file associated with the name
-                // to open a file use
-                // System.Diagnostics.Process.Start(@"|DataDirectory|\\Resources\\files\\" + selectedItem.Index);
-                FileList.Items.Clear();
-                DataTable files = new DataTable();
-                Set_Command("SELECT * FROM year WHERE Id='" + selectedItem.Index + "'");
-                connection.Open();
-                _ = da.Fill(files);
+                Source = new BitmapImage(new Uri(Environment.CurrentDirectory + "\\Resources\\" + "\\Images\\" +
+                                                 "\\adobepdfimage.png"))
+            };
 
-                foreach (DataRow dtrow in files.Rows)
+            foreach (DataRow dtrow in files.Rows)
+            {
+                FileList.Items.Add(new ListOfFiles()
                 {
-                    _ = FileList.Items.Add(dtrow["filename"].ToString());
-                }
-                connection.Close();
+                    PathOfFile = dtrow["filename"].ToString(),
+                    AdobePdfIcon = x
+                });
             }
+
+            connection.Close();
         }
 
         private void FileList_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             // just to show how to access the filename displayed on the list
-            _ = MessageBox.Show(FileList.SelectedItem.ToString());
+            var x = FileList.SelectedItem as ListOfFiles;
+            MessageBox.Show(x?.PathOfFile);
         }
     }
 
     public class MenuItem
     {
-
         public ObservableCollection<MenuItem> Items { get; set; }
 
         public MenuItem()
@@ -117,5 +124,11 @@ namespace notes_app_csharp_wpf.Pages
         public string Title { get; set; }
 
         public string Index { get; set; }
+    }
+
+    public class ListOfFiles
+    {
+        public string PathOfFile { get; set; }
+        public Image AdobePdfIcon { get; set; }
     }
 }
