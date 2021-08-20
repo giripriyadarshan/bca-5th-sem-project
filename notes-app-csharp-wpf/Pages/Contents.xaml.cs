5ecdd36121321d8e01777d92b7291fe1c9a2ce50
Giri Priyadarshan <43517605+giripriyadarshan@users.noreply.github.com>
2021-08-20 15:34:39 +0530

    schema designed
    schema designed ...... probably will work properly

--- notes-app-csharp-wpf/Pages/Contents.xaml.cs
+++ notes-app-csharp-wpf/Pages/Contents.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Collections.Generic;
 using System.Data;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,6 +19,78 @@ namespace notes_app_csharp_wpf.Pages
             InitializeComponent();
         }
 
+        //private HashSet<string> semesterSet = new HashSet<string>();
+
+        /*
+        
+        For every sem, add the subjects
+        For every subject in a sem, add the years
+        For every year in a sub, for a sem, add the papers
+
+        Sem
+        : primary key - sem numbers
+        
+        Sub
+        : ID
+        : subject names
+        : foriegn key - sem number
+        (No duplications of entire row)
+        Method?
+            : if select * from sub where subjectName=@subname and semNumber=@semnunmber returns 0 rows, then INSERT values
+        and extract the ID of the inserted row
+        else
+        extract the ID of the existing row
+        
+        Year
+        : ID
+        : year numbers
+        : subID Foriegn Key
+        (No duplications of entire row)
+        Method? 
+            : if SELECT * FROM year WHERE sem=@semester AND sub=@subject returns 0 rows, then INSERT INTO year (year, sem, sub) VALUES (@year, @sem, @sub)
+        and extract the ID of the inserted row
+        else
+        extract the ID of the existing row
+
+        Files
+        : ID
+        : Name
+        : yearID  
+
+        */
+
+        /*
+
+        //////////////////////////////////////////////////////////
+        /// probable method to implement collections
+        
+        ' select * from sem '
+
+        for each row in sem
+            add sem to list
+            ' select * from sub where sem number = sem '
+
+            for each row in sub
+                add sub to sem's list
+                ' select * from year where subID = ID '
+
+                for each row in year
+                    add year to sub's list
+
+        */
+
+        /*
+                    
+        // list all files
+
+        ' select * from files '
+        ListOfFiles.Title = row["name"]
+        ListOfFiles.yearID = row["yearID"]
+        // reason for yearId in the option?
+        segregate the file storage in folders named using yearID to avoid confusion among files with similar name
+
+         */
+
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
             if (connection.State == ConnectionState.Closed)
@@ -28,29 +101,33 @@ namespace notes_app_csharp_wpf.Pages
             var sem = new MenuItem() { Title = "Semester" };
 
             var dt = new DataTable();
-            Set_Command("SELECT * FROM semester");
+            Set_Command("SELECT * FROM semester ORDER BY semester ASC");
             _ = da.Fill(dt);
 
             foreach (DataRow dtrow in dt.Rows)
             {
-                var sub = new MenuItem()
-                {
-                    Title = dtrow["semester"].ToString(),
-                };
+                
+                
+                    var sub = new MenuItem()
+                    {
+                        Title = dtrow["semester"].ToString(),
+                    };
+                
 
-                Set_Command("SELECT * FROM subject WHERE semesterID=" + dtrow["Id"]);
+                Set_Command("SELECT subject_name, Id, semesterID FROM subject WHERE semesterID='" + dtrow["Id"] + "' GROUP BY subject_name");
                 var s = new DataTable();
                 _ = da.Fill(s);
 
                 foreach (DataRow dtrow2 in s.Rows)
                 {
-                    var year = new MenuItem()
-                    {
-                        Title = dtrow2["subject_name"].ToString(),
-                    };
+                    
+                        var year = new MenuItem()
+                        {
+                            Title = dtrow2["subject_name"].ToString(),
+                        };
 
                     Set_Command(
-                        "SELECT * FROM year WHERE semesterID=" +
+                        "SELECT DISTINCT year FROM year WHERE semesterID=" +
                         dtrow2["semesterID"] +
                         " AND subjectID=" + dtrow2["Id"]
                     );
@@ -61,28 +138,21 @@ namespace notes_app_csharp_wpf.Pages
                     {
                         year.Items.Add(new MenuItem()
                         {
-                            Title = dtrow3["year"].ToString() + dtrow3["filename"],
-                            Index = dtrow3["Id"].ToString()
-                        });
-
-                        // list all the files during load
-                        var x = new Image
-                        {
-                            Source = new BitmapImage(new Uri(Environment.CurrentDirectory + "\\Resources\\" + "\\Images\\" +
-                                                "\\adobepdfimage.png"))
-                        };
-                        _ = FileList.Items.Add(new ListOfFiles()
-                        {
-                            PathOfFile = dtrow3["filename"].ToString(),
-                            AdobePdfIcon = x
+                            Title = dtrow3["year"].ToString(),
+                            Sem = dtrow2["semesterID"].ToString(),
+                            Sub = dtrow2["Id"].ToString()
                         });
 
                     }
 
-                    sub.Items.Add(year);
+                    
+                        sub.Items.Add(year);
+                    
                 }
 
-                sem.Items.Add(sub);
+                
+                    sem.Items.Add(sub);
+                
             }
 
             _ = MainList.Items.Add(sem);
@@ -93,10 +163,12 @@ namespace notes_app_csharp_wpf.Pages
         private void MainList_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
         {
             if (!(e.NewValue is MenuItem selectedItem)) return;
-            
+
+            if (selectedItem.Sem == null) return;
+
             FileList.Items.Clear();
             var files = new DataTable();
-            Set_Command("SELECT * FROM year WHERE Id='" + selectedItem.Index + "'");
+            Set_Command("SELECT * FROM year WHERE semesterID='" + selectedItem.Sem + "' AND subjectID='" + selectedItem.Sub + "' AND year='" + selectedItem.Title + "'");
             connection.Open();
             _ = da.Fill(files);
             var x = new Image
@@ -139,7 +211,8 @@ namespace notes_app_csharp_wpf.Pages
 
         // change index to sem and year seperately
         // the purpose is to call the "year" query filtering duplicates and store the sem and sub to list all the files in that year
-        public string Index { get; set; }
+        public string Sem { get; set; }
+        public string Sub { get; set; }
     }
 
     public class ListOfFiles
