acad798fcb37f1e7244221e2627d8434e171a1d9
Giri Priyadarshan <43517605+giripriyadarshan@users.noreply.github.com>
2022-02-15 14:35:33 +0530

    reports feature implemented

--- PYQ_Papers/Pages/ContentDetails.xaml.cs
+++ PYQ_Papers/Pages/ContentDetails.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static PYQ_Papers.commons;
 
 namespace PYQ_Papers.Pages
 {
@@ -23,6 +25,64 @@ namespace PYQ_Papers.Pages
         public ContentDetails()
         {
             InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Files(ref Ascending, true);
+            Files(ref Descending, false);
+        }
+
+        private void Files(ref ListBox fileList, bool isAscending)
+        {
+            if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+
+            var files = new DataTable();
+            var commandString = "SELECT * FROM files ORDER BY no_of_times_opened " + (isAscending ? "ASC " : "DESC ");
+            Set_Command(commandString);
+            _ = da.Fill(files);
+            fileList.Items.Clear();
+
+            foreach (DataRow dtrow in files.Rows)
+            {
+                fileList.Items.Add(new ListOfFiles()
+                {
+                    PathOfFile = dtrow["file_name"].ToString(),
+                    YearID = dtrow["yearID"].ToString(),
+                    OpenedCount = dtrow["no_of_times_opened"].ToString(),
+                });
+            }
+
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+
+        private void Descending_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (Descending.SelectedItem == null) return;
+
+            var x = Descending.SelectedItem as ListOfFiles;
+
+            _ = System.Diagnostics.Process.Start(Set_File_Storage_String(int.Parse(x.YearID)) + x.PathOfFile);
+        }
+
+        private void Ascending_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (Ascending.SelectedItem == null) return;
+
+            var x = Ascending.SelectedItem as ListOfFiles;
+
+            _ = System.Diagnostics.Process.Start(Set_File_Storage_String(int.Parse(x.YearID)) + x.PathOfFile);
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            _ = NavigationService?.Navigate(new PostLoginMenuPage());
         }
     }
 }
